4b82834103b51cac42f9ab706a7f70de9175bb3d
fpAmin <fpaminmennaoui@gmail.com>
2025-05-08 20:08:12 +0200

    Create Sistemas.java

---
+++ Sistemas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemas;
+
+/**
+ *
+ * @author mmenben188
+ */
+public class Sistemas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int op = 0;
+        while  (op!=1)
+        {
+            System.out.println("Hola mundo ");
+        }
+    }
+    
+}
